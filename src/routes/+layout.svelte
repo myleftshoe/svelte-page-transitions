225c35f69d8a0e70d682e0d5684f09675e8f9d35
myleftshoe <paulesammut@gmail.com>
2022-12-26 10:38:44 +1100

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,13 +14,7 @@
 </script>
 
 <div id="app">
-    <nav>
-        <a href="#" on:click={goBack}>Back</a> | <a href="/page1">Page 1</a> | <a href="/page2">Page 2</a>
-    </nav>
-    <main>
-        <slot/>
-    </main>
-    <footer>Footer</footer>
+    <slot/>
 </div>
 
 <style>
@@ -36,23 +30,5 @@
             'nav' 3rem
             'main' 1fr
             'footer' 3rem;
-    }
-    nav {
-        grid-area: nav;
-    }
-    main {
-        grid-area: main;
-        display: grid;
-        grid-template-rows: 1fr;
-        grid-template-columns: 1fr;
-    }
-    /* use :global since the Route element may be in another component */
-    main > :global(*) {
-        grid-row: 1;
-        grid-column: 1;
-    }
-    footer { 
-        grid-area: footer;
-        background-color: cyan;
     }
 </style>
--- src/routes/page2/+page.svelte
+++ src/routes/page2/+page.svelte
@@ -1,31 +1,56 @@
 <script>
+    import { fly, scale } from 'svelte/transition'
     import Page from '$lib/components/page.svelte'
-    const items = Array(10).fill('Item').map((item, index) => `${item} ${index}`)
+    const items = Array(4).fill('Item').map((item, index) => `${item} ${index}`)
 </script>
 
-<Page>
-    <section>
-        <h1>PAGE 2</h1>
-        {#each items as item}
-            <item>{item}</item>
-        {/each}
-    </section>
-</Page>    
-
+<nav>
+    <div in:fly={{x:375}} out:fly={{x:-375, duration: 4000}}>
+        <a href="#">Back</a> | <a href="/page1">Page 1</a> | <a href="/page2">Page 2</a>
+    </div>
+</nav>
+<main in:fly={{x:375}} out:scale>
+    <h1>PAGE 2</h1>
+    {#each items as item}
+        <item>{item}</item>
+    {/each}
+</main>
+<footer>
+    <div in:fly={{y:40}} out:fly={{y:40, duration: 4000}}>
+        Footer
+    </div>
+</footer>
 <style>
-    section {
+    div { 
+        background-color: red;
+        z-index: 10;
+    }
+    nav {
+        grid-area: nav;
+        display: grid;
+        place-content: center;
+        background-color: cyan;
+    }
+    main {
+        grid-area: main;
         display:flex;
         flex-direction: column;
         align-items: center;        
+        background-color: #0f07;
     }
     item {
         color: black;
         display: block; 
-        background-color: white;
+        background-color: #00f7;
         margin: 1px;
         padding: 8px;
         width: 90%;
         text-align: center;
-        /* width: 100%; */
+    }
+    footer {
+        grid-area: footer;
+        display: grid;
+        place-content: center;
+        background-color: pink;
     }
 </style>
